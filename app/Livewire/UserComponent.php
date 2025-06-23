3634895cdbccc6c3d48d00933bbad828ad9d804e
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-06-23 18:38:27 +0700

    crud user

--- app/Livewire/UserComponent.php
+++ app/Livewire/UserComponent.php
@@ -9,11 +9,41 @@ use Livewire\WithPagination;
 
 class UserComponent extends Component
 {
-    use WithPagination,WithoutUrlPagination;
+    use WithPagination, WithoutUrlPagination;
+    public $nama, $email, $password;
     public function render()
     {
         // guanakan fitur pagination
         $data['user'] = User::paginate(10);
-        return view('livewire.user-component',$data);
+        return view('livewire.user-component', $data);
+    }
+
+    public function tambahUser()
+    {
+        $this->validate([
+            'nama' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8'
+        ], [
+            'nama.required' => 'Nama tidak boleh kosong',
+            'nama.string' => 'Nama tidak boleh berupa angka',
+
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Format email salah',
+            'email.unique' => 'Email sudah digunakan',
+
+            'password.required' => 'Password tidak boleh kosong',
+            'password.min' => 'Password harus lebih dari 8 karakter',
+        ]);
+
+        User::create([
+            'nama' => $this->nama,
+            'email' => $this->email,
+            'jenis' => 'admin',
+            'password' => $this->password,
+        ]);
+        // membuat session
+        session()->flash('success','Berhasil menambah user');
+        $this->reset();
     }
 }
--- resources/views/livewire/user-component.blade.php
+++ resources/views/livewire/user-component.blade.php
@@ -1,7 +1,71 @@
 <div>
     {{-- In work, do what you enjoy. --}}
-    <h1>Ini User Page</h1>
+    <h1 class="text-black-800 text-center border border-blue-400 rounded-full font-bold mb-3">Kelola User</h1>
+    <hr>
     {{-- Table User --}}
+    {{-- Tombol Tambah --}}
+    <button class="btn btn-sm btn-info mb-2 mt-2" onclick="my_modal_3.showModal()">Tambah User</button>
+    {{-- alert berhasi tambah user --}}
+    @if(session()->has('success'))
+    <div role="alert" class="alert alert-success">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>{{session('success')}}</span>
+    </div>
+    @endif
+    {{-- Modal Tambah User --}}
+    <dialog id="my_modal_3" class="modal">
+        <div class="modal-box">
+            <form>
+                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-md border p-4">
+                    <legend class="fieldset-legend">Tambah User</legend>
+                    <label class="label">Nama</label>
+                    <input type="text" class="input w-auto" wire:model='nama' placeholder="Masukan nama."
+                        value="{{@old('nama')}}" />
+                    @error('nama')
+                    <div role="alert" class="alert alert-error mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>{{$message}}</span>
+                    </div>
+                    @enderror
+                    <label class="label">Email</label>
+                    <input type="email" class="input w-auto" wire:model='email' placeholder="contoh@email.com"
+                        value="{{@old('')}}" />
+                    @error('email')
+                    <div role="alert" class="alert alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>{{$message}}</span>
+                    </div>
+                    @enderror
+                    <label class="label">Password</label>
+                    <input type="password" class="input w-auto" wire:model='password' placeholder="8 karakter/lebih"
+                        value="{{@old('')}}" />
+                    @error('password')
+                    <div role="alert" class="alert alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>{{$message}}</span>
+                    </div>
+                    @enderror
+                </fieldset>
+                <button type="button" wire:click='tambahUser' class="btn btn-sm btn-info mt-2 ">Tambah</button>
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+        </div>
+    </dialog>
     <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
         <table class="table">
             <!-- head -->
@@ -13,6 +77,7 @@
                     <th>Alamat</th>
                     <th>Telepon</th>
                     <th>Jenis</th>
+                    <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
@@ -25,6 +90,8 @@
                     <td>{{$users->alamat}}</td>
                     <td>{{$users->telepon}}</td>
                     <td>{{$users->jenis}}</td>
+                    <td><a href="#" class="btn btn-xs btn-dash btn-info">Edit</a> <a href="#"
+                            class="btn btn-xs btn-dash btn-error">Hapus</a></td>
                 </tr>
                 @endforeach
                 {{ $user->links() }}
